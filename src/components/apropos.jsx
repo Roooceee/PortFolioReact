702272714f4a734de835978a8eddceb787fa8b09
Sebastien Lucas <sebastien.jose.lucas@gmail.com>
2025-04-17 15:37:53 +0100

    optimisation lighthouse

--- src/components/apropos.jsx
+++ src/components/apropos.jsx
@@ -6,7 +6,7 @@ function APropos({id},ref) {
       return (
          <section id='apropos'>      
             <div>
-                  <img src="/img/image_profil_SL.png" alt="image de profil" />
+                  <img src="/img/image_profil_SL.webp" alt="image de profil" />
                <div>
                   <h2 ref={ref} className='title-section'>A Propos</h2>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptatem corrupti eos voluptatibus eum fugit nam possimus adipisci sed eaque exercitationem et, fugiat excepturi expedita dicta aperiam delectus doloribus earum.</p>
